90377749749b85cd3e57f62946e0d8a827d43e39
Wanghsaopeng <483041241@qq.com>
2022-08-07 13:56:27 +0800

    完成生成炸弹以及自动翻开0

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,124 @@
-<script setup lang="ts">
-const name = $ref('')
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-</script>
-
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+    Minesweeper
+    <div p5 select-none>
+      <div flex="~" items-center justify-center v-for="row, x in state" :key="x">
+        <button :class="getBlockClass(block)" @click="onClick(block)" v-for="block, y in row" :key="y"
+          hover="bg-gray/100" w-8 h-8 border="1 gray-400/5" flex="~" items-center justify-center m="0.5">
+          <template v-if="block.revealed || dev">
+            <div v-if='block.mine' i-mdi:mine></div>
+            <div v-else>{{ block.adjacentMines }}</div>
+          </template>
+        </button>
+      </div>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines: number
+}
+const WIDTH: number = 10;
+const HEIGHT: number = 10;
+const state = $ref(
+  Array.from({ length: HEIGHT }, (_, y) =>
+    Array.from({ length: WIDTH },
+      (_, x): BlockState => ({
+        x,
+        y,
+        adjacentMines: 0
+      })
+    ))
+)
+const directions = [
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1]
+]
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-green-500',
+  'text-yellow-500',
+  'text-orange-500',
+  'text-red-500',
+  'text-purple-500',
+  'text-pink-500',
+  'text-teal-500',
+]
+let mineGenerate = false;
+let dev = true;
+function getBlockClass(block: BlockState) {
+  if (!block.revealed) return 'bg-gray-500/10'
+  return block.mine ? 'text-#fab' : numberColors[block.adjacentMines]
+}
+function updateNumber() {
+  state.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine) return
+      getSiblings(block).forEach((item) => {
+        if (item.mine)
+          block.adjacentMines++;
+      });
+    })
+  })
+}
+function getSiblings(block: BlockState) {
+  return directions.map(([dx, dy]) => {
+    const xx = block.x + dx;
+    const yy = block.y + dy;
+    if (xx < 0 || xx >= WIDTH || yy < 0 || yy >= HEIGHT)
+      return undefined;
+    return state[yy][xx]
+  }).filter(Boolean) as BlockState[];
+}
+function generateMines(initial: BlockState) {
+  for (const row of state) {
+    for (const block of row) {
+      if (Math.abs(initial.x - block.x) <= 1 || Math.abs(initial.y - block.y) <= 1) {
+        continue;
+      }
+      block.mine = Math.random() < 0.2;
+    }
+  }
+  updateNumber();
+}
+function expendZero(block: BlockState) {
+  if (block.adjacentMines) return;
+  getSiblings(block).forEach(item => {
+    if (!item.revealed) {
+      item.revealed = true;
+      expendZero(item);
+    }
+  });
+}
+function onClick(block: BlockState) {
+  if (!mineGenerate) {
+    mineGenerate = true;
+    generateMines(block);
+  }
+  expendZero(block);
+  block.revealed = true;
+  if (block.mine) alert('BOOOM!')
+}
+</script>
+
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
